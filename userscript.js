3bd1fce0d1a6cd9055819ea07f9194cf95ccb895
Mihail Nica <110503052+mihailnicamn@users.noreply.github.com>
2022-11-20 19:07:08 +0200

    clean 🐝

--- userscript.js
+++ userscript.js
@@ -8,18 +8,42 @@
 // @grant        none
 // ==/UserScript==
 
-(async function() {
+(async function () {
     'use strict';
-    const elements_array_url = 'https://raw.githubusercontent.com/mihailnicamn/ad_blocker_elements/main/elemets.json' 
+    const elements_array_url = 'https://raw.githubusercontent.com/mihailnicamn/ad_blocker_elements/main/elemets.json'
     const elements_array = await fetch(elements_array_url).then(response => response.json())
     setInterval(() => {
-    for (let i = 0; i < elements_array.length; i++) {
-        const element = elements_array[i]
-        const element_to_remove = document.querySelector(element)
-        if (element_to_remove) {
-            element_to_remove.remove()
+        for (let i = 0; i < elements_array.length; i++) {
+            const element = elements_array[i]
+            if (element.includes('.')) {
+                var __ = element.replace('.', '')
+                var _ = document.getElementsByClassName(__)
+                if(_) {
+                    console.log('found element with class name: ' + __)
+                for (let i = 0; i < _.length; i++) {
+                    const element = _[i];
+                    element.remove()
+                }
+            }
+            } else if (element.includes('#')) {
+                var __ = element.replace('#', '')
+                var _ = document.getElementById(__)
+                if (_) {
+                    console.log('found element with id: ' + __)
+                    _.remove()
+                }
+            } else {
+                var _ = document.getElementsByTagName(element)
+                if (_) {
+                    console.log('found element with tag name: ' + element)
+                    for (let i = 0; i < _.length; i++) {
+                        const element = _[i];
+                        element.remove()
+                    }
+                }
+            }
         }
-    }
+        console.log("ad blocker is running")
     }, 1000)
 
 })();
